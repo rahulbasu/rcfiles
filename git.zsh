b54a36f8e4f4d7470dd630de673bc63a4668fbb1
Rahul Basu <rahul.basu@flexport.com>
2018-09-18 11:59:28 -0700

    git and flexport

---
+++ git.zsh
@@ -0,0 +1,2 @@
+alias mergemaster='git co master && git pull origin master && git co - && git merge master'
+alias rebasemaster='git co master && git pull origin master && git co - && git rebase master'
